4371eec5bfac3f1e5eaf3f82bed082ea49e6ce34
JonasKaruzas <jonas.karuzas@gmail.com>
2021-10-08 17:20:13 +0300

    less steps

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,29 +1,20 @@
 def bubble_sort(arr)
   n = arr.length-1
+  inner = n
 
   for j in 1..n do
-    for i in 1..n do
-      
+    for i in 1..inner do
         if arr[i-1] > arr[i]
-          p "------"
-          p arr
-
           temp = arr[i]
           arr[i] = arr[i-1]
           arr[i-1] = temp
-
         end
-
-        p arr
-        p "------"
-      
     end
+    inner -= 1
   end
+  p arr
 end
 
 
-
-bubble_sort([5,4,3,2,1])
-
-# bubble_sort([4,3,78,2,0,2])
+bubble_sort([4,3,78,2,0,2])
 # => [0,2,2,3,4,78]
